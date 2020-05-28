7f4a0c4d7df6b4c6b9e745c33b88fe04dfbace80
amrin <amrin.wan@gmail.com>
2020-05-29 01:07:34 +0800

    Add project files.

---
+++ Translation Fee Calculator/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Translation_Fee_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rateBox = new System.Windows.Forms.TextBox();
+            this.pageBox = new System.Windows.Forms.TextBox();
+            this.disBox = new System.Windows.Forms.TextBox();
+            this.disFromBox = new System.Windows.Forms.TextBox();
+            this.totalLbl = new System.Windows.Forms.Label();
+            this.calBtn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Translation Fee Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rate per Page";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Page Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Discount";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Discount From";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total Amount";
+            // 
+            // rateBox
+            // 
+            this.rateBox.Location = new System.Drawing.Point(179, 67);
+            this.rateBox.Name = "rateBox";
+            this.rateBox.Size = new System.Drawing.Size(40, 20);
+            this.rateBox.TabIndex = 6;
+            // 
+            // pageBox
+            // 
+            this.pageBox.Location = new System.Drawing.Point(179, 97);
+            this.pageBox.Name = "pageBox";
+            this.pageBox.Size = new System.Drawing.Size(40, 20);
+            this.pageBox.TabIndex = 7;
+            // 
+            // disBox
+            // 
+            this.disBox.Location = new System.Drawing.Point(179, 125);
+            this.disBox.Name = "disBox";
+            this.disBox.Size = new System.Drawing.Size(40, 20);
+            this.disBox.TabIndex = 8;
+            // 
+            // disFromBox
+            // 
+            this.disFromBox.Location = new System.Drawing.Point(179, 153);
+            this.disFromBox.Name = "disFromBox";
+            this.disFromBox.Size = new System.Drawing.Size(40, 20);
+            this.disFromBox.TabIndex = 9;
+            // 
+            // totalLbl
+            // 
+            this.totalLbl.AutoSize = true;
+            this.totalLbl.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLbl.Location = new System.Drawing.Point(191, 188);
+            this.totalLbl.Name = "totalLbl";
+            this.totalLbl.Size = new System.Drawing.Size(19, 21);
+            this.totalLbl.TabIndex = 10;
+            this.totalLbl.Text = "0";
+            // 
+            // calBtn
+            // 
+            this.calBtn.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calBtn.Location = new System.Drawing.Point(45, 228);
+            this.calBtn.Name = "calBtn";
+            this.calBtn.Size = new System.Drawing.Size(164, 49);
+            this.calBtn.TabIndex = 11;
+            this.calBtn.Text = "Calculate";
+            this.calBtn.UseVisualStyleBackColor = true;
+            this.calBtn.Click += new System.EventHandler(this.calBtn_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(175, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 21);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(254, 321);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.calBtn);
+            this.Controls.Add(this.totalLbl);
+            this.Controls.Add(this.disFromBox);
+            this.Controls.Add(this.disBox);
+            this.Controls.Add(this.pageBox);
+            this.Controls.Add(this.rateBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TFC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox rateBox;
+        private System.Windows.Forms.TextBox pageBox;
+        private System.Windows.Forms.TextBox disBox;
+        private System.Windows.Forms.TextBox disFromBox;
+        private System.Windows.Forms.Label totalLbl;
+        private System.Windows.Forms.Button calBtn;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Translation Fee Calculator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translation_Fee_Calculator
+{
+    public partial class Form1 : Form
+    {
+        int rate, pageCount, dis, from;
+        int extra, page, freq, total_amount;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rateBox.Text = "1";
+            disBox.Text = "5";
+            disFromBox.Text = "30";
+        }
+
+        private void calBtn_Click(object sender, EventArgs e)
+        {
+            rate = int.Parse(rateBox.Text);
+            pageCount = int.Parse(pageBox.Text);
+            dis = int.Parse(disBox.Text);
+            from = int.Parse(disFromBox.Text);
+
+            total_amount = pageCount * rate;
+            if (pageCount >= from)
+            {
+                extra = pageCount % (from - 10);
+                page = pageCount - extra;
+                freq = page / (from - 10);
+                dis *= freq;
+                total_amount = page - dis + extra;
+            }
+
+            totalLbl.Text = total_amount.ToString();
+        }
+    }
+}
